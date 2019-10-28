e5e86c1f1a38d3fce44ad70c9a42bceaa53eadcc
choiho870712 <choiho870712@gmail.com>
2019-10-28 20:07:18 +0800

    .

---
+++ linux/env.py
@@ -0,0 +1,131 @@
+# find process id with psutil
+# https://thispointer.com/python-check-if-a-process-is-running-by-name-and-find-its-process-id-pid/
+import psutil
+
+def findProcessIdByName(processName):
+    listOfProcessObjects = []
+
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
+            # Check if process name contains the given name string.
+            if processName.lower() in pinfo['name'].lower() :
+                listOfProcessObjects.append(pinfo)
+        except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
+            pass
+
+    return listOfProcessObjects[0]['pid']
+
+# read memory with ptrace
+# https://www.ownedcore.com/forums/world-of-warcraft/world-of-warcraft-bots-programs/wow-memory-editing/330612-linux-wine-reading-memory-ptrace.html 
+from ptrace.debugger.debugger import PtraceDebugger
+from ptrace.debugger.process import PtraceProcess
+from struct import unpack
+
+class memory_reader :
+
+    def __init__(self, pid) :
+        self.tracer = PtraceProcess(PtraceDebugger(), pid, False)
+
+    def read_int4(self, address) :
+        return unpack("I", self.tracer.readBytes(address, 4))[0]
+
+    def search_address(self, low_address, high_address, target_value1, target_value2) :
+        addr = low_address
+        value = self.read_int4(addr)
+        while addr <= high_address :
+            addr += 0x4
+            pre_value = value
+            value = self.read_int4(addr)
+            if pre_value == target_value1 and value == target_value2 :
+                return addr - 0x4
+
+        return 0
+
+import control as c
+import numpy as np
+import time
+
+class Env :
+
+    def __init__(self) :
+        self.reader = memory_reader(findProcessIdByName('volleyball'))
+        self.caculate_addresses()
+        self.action_space = {0: c.release,1: c.left,2: c.right,3: c.up,4: c.down,5: c.p}
+        self.state_space_num = 16
+        self.action_space_num = 6
+        self.cur_state = np.array(self.sub_state())
+        self.pre_state = self.cur_state
+        self.time_stamp = time.time()
+        print("envrionment is ready!")
+
+    def caculate_addresses(self) :
+        # check memory value with scanmem
+        # read memory with ptrace
+        # https://www.unknowncheats.me/forum/other-software/151529-scanmem-value-editing-installation-tutorial-linux-2-a.html
+        # https://wiki.ubuntu.com/SecurityTeam/Roadmap/KernelHardening#ptrace_Protection
+        # player2.y   // init = 36  // range = 32  ~ 184
+        # player2.x   // init = 244 // range = 108 ~ 244
+        # player1.y   // init = 396 // range = 248 ~ 400
+        # player1.x   // init = 244 // range = 108 ~ 244
+        # ball.y      // init = 56  // range = 20  ~ 430
+        # ball.x      // init = 0   // range = 0   ~ 252
+        # flag        // 0 : a game start,  1 : ball start, 2 : playing ball, 3 : ball end, 4 : game set, 10 : begin screen
+        self.player1_y_address = self.reader.search_address(0x5a0000, 0x5b0000, 396, 244)
+        self.player1_x_address = self.player1_y_address + 0x4
+        self.base = self.player1_y_address - (self.player1_y_address & 0xffff)
+        self.player2_y_address = self.reader.search_address(self.base, self.base + 0xffff, 36, 244)
+        self.player2_x_address = self.player2_y_address + 0x4
+        self.ball_y_address = self.reader.search_address(self.base + 0x6000, self.base + 0xffff, 56, 0)
+        self.ball_x_address = self.ball_y_address + 0x4
+        self.flag_address = self.reader.search_address(self.base + 0xe00, self.base + 0xf00, 0, 15) - 0x4
+
+    def release_key(self) :
+        c.release()
+
+    def sub_state(self) :
+        # sub_state = [p2.y, p2.x, p1.y, p1.x, b.y, b.x]
+        s = []
+        s.append(self.reader.read_int4(self.player2_y_address))
+        s.append(self.reader.read_int4(self.player2_x_address))
+        s.append(self.reader.read_int4(self.player1_y_address))
+        s.append(self.reader.read_int4(self.player1_x_address))
+        s.append(self.reader.read_int4(self.ball_y_address))
+        s.append(self.reader.read_int4(self.ball_x_address))
+        return np.array(s)
+
+    def state(self) :
+        # state = [sub_state, vector, key_map]
+        # sub_state = [p2.y, p2.x, p1.y, p1.x, b.y, b.x]
+        # vector = sub_state - sub_state_before_0.1s
+        # key_map = [left, right, up, down]
+        if time.time() - self.time_stamp > 0.1 :
+            self.pre_state = self.cur_state
+            self.cur_state = self.sub_state()
+            vector = self.cur_state - self.pre_state
+            self.time_stamp = time.time()
+            return np.concatenate((self.pre_state, vector, c.get_key_map()), axis=None), True
+        else :
+            return np.zeros(16), False
+
+    def flag(self) :
+        # 0 : a game start,  1 : ball start, 2 : playing ball, 3 : ball end, 4 : game set, 10 : begin screen
+        return self.reader.read_int4(self.flag_address)
+
+    def step(self, action) :
+        # action = [release, left, right, up, down, power]
+        self.action_space[action]()
+
+# test environment
+if __name__ == "__main__":
+    env = Env()
+    while True :
+        # # test flag
+        # print(env.flag())
+
+        # test state
+        state, got_state = env.state()
+        if got_state :
+            print(state)
+
